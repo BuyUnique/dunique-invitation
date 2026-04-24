b5d2b4cdd5bd113b2bff5b729c8112f90b6db3fa
jonvardanyan99 <john.vardanyan99@gmail.com>
2026-04-24 13:34:33 +0400

    Improve style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,7 +6,7 @@ import arrowDownIcon from '../../assets/icons/arrow-down.png';
 
 export const Header: React.FC = () => {
   return (
-    <header style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), url(${weddingImg})` }} className="relative w-full h-screen flex justify-center items-center bg-cover bg-center">
+    <header style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), url(${weddingImg})` }} className="relative w-full h-dvh flex justify-center items-center bg-cover bg-center">
       <h1 className="text-[100px] italic text-white max-[800px]:text-[60px] max-[500px]:text-[40px]">Ֆելիքս և Ամալյա</h1>
       <div style={{ backgroundImage: `url(${curtainWhiteImg})` }} className="absolute top-0 left-0 w-[50%] h-full bg-cover z-2 animate-curtain-opening origin-left" />
       <div style={{ backgroundImage: `url(${curtainWhiteImg})` }} className="absolute top-0 right-0 w-[50%] h-full bg-cover z-2 animate-curtain-opening origin-right" />
