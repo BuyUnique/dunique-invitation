cb1f653d9213be73752f5faf2fb0af90adc5c50c
BuyUnique <tereza.vardanyan555@gmail.com>
2026-05-29 17:04:55 +0400

    commit

--- src/constants/weddingDatas.tsx
+++ src/constants/weddingDatas.tsx
@@ -5,36 +5,36 @@ import brunoMarsSong from '../assets/audios/Bruno Mars - Merry You.mp3';
 
 export const WEDDING_DATAS: WeddingData[] = [
   {
-    id: 'FelixAndAmalya',
+    id: 'DavidAndHasmik',
     img: felixAndAmalyaImg,
     song: brunoMarsSong,
-    title: 'Ֆելիքս և Ամալյա',
-    weddingDateText: '21 Հունիս 2026',
-    weddingDate: new Date(2026, 5, 21),
+    title: 'Դավիթ և Հասմիկ',
+    weddingDateText: '14 Հունիս 2026',
+    weddingDate: new Date(2026, 5, 14),
     programEvents: [
       {
         name: 'brideHouse',
         time: '11:30',
-        locationText: 'Կոմիտաս 51',
-        location: 'https://yandex.com/maps/10262/yerevan/house/YE0YcwZoSUcBQFpqfX5xdnxnYQ==/?ll=44.518026%2C40.207045&utm_source=share&z=17.05',
+        locationText: 'Կոմիտաս 12',
+        location: 'https://yandex.com/maps/-/CPwXr-kX',
       },
       {
         name: 'church',
         time: '16:00',
-        place: 'Սաղմոսավանք',
-        locationText: 'Արագածոտնի մարզ, գ. Սաղմոսավան',
-        location: 'https://yandex.com/maps/org/8680895053/?ll=44.394665%2C40.380342&utm_source=share&z=16.87',
+        place: 'Խոր Վիրապ վանք',
+        locationText: 'Արարատի մարզ, գ. Խոր Վիրապ',
+        location: 'https://yandex.com/maps/-/CPwXvW-s',
       },
       {
         name: 'party',
-        time: '17:45',
+        time: '17:30',
         place: '«Ֆլորենս» ռեստորան',
         locationText: 'Բարբյուսի փող., 64/2 շենք',
         location: 'https://yandex.com/maps/org/46488697731/?ll=44.481653%2C40.194364&utm_source=share&z=17',
       },
     ],
     infoMessage: 'Փոքրիկ հյուրերին խնդրում ենք մինչև ժամը 22:30 մնալ ներքևի սրահում՝ անվտանգության նկատառումներից ելնելով: Մենք հոգ ենք տարել մեր փոքրիկ հյուրերի հանգստի մասին․ ներքևի հատուկ սրահում նրանց սպասվում է հետաքրքիր ժամանցային ծրագիր։',
-    dressCodeMessage: 'Հանդիսավոր հագուստ: Կանանց համար երեկոյան հագուստ։ Տղամարդկանց համար փողկապը պարտադիր չէ։ Խնդրում ենք ընտրել հագուստ՝ բացառելով սպիտակի և բուրգունդիի երանգները։',
+    dressCodeMessage: 'Խնդրում ենք ընտրել հանդիսավոր հագուստ շագանակագույնի կամ կանաչ երագների մեջ ՝ բացառելով սպիտակ գույնի օգտագործումը։',
     phoneNumbers: ['098 123 456', '077 123 456'],
   },
 ];
