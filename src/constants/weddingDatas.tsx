45912ff53009345b19bb33522a30b54d848ec581
BuyUnique <tereza.vardanyan555@gmail.com>
2026-09-20 18:25:18 +0400

    commit

--- src/constants/weddingDatas.tsx
+++ src/constants/weddingDatas.tsx
@@ -7,7 +7,7 @@ export const WEDDING_DATAS: WeddingData[] = [
   {
     id: 'LiannaAndDavit',
     img: LiannaAndDavit,
-    imgPosition: '80% 50%',
+    imgPosition: '80% 80%',
     song: brunoMarsSong,
     primaryColor: '#00a63d',
     title: 'Լիաննա և Դավիթ',
